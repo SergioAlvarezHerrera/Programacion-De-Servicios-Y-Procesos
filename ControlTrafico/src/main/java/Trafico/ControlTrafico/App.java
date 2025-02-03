f984d733f1e1d6542f7a84854f6c65bb85f58441
SergioAlvarezHerrera <sergioalv1000@gmail.com>
2025-02-03 15:52:21 +0100

    Control Trafico

--- ControlTrafico/src/main/java/Trafico/ControlTrafico/App.java
+++ ControlTrafico/src/main/java/Trafico/ControlTrafico/App.java
@@ -1,13 +1,8 @@
 package Trafico.ControlTrafico;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	public static void main(String[] args) {
+        Cruce cruce = new Cruce(10);
+        cruce.iniciar();
     }
 }
--- ControlTrafico/src/main/java/Trafico/ControlTrafico/SimulacionSemaforos.java
+++
@@ -1,9 +0,0 @@
-package Trafico.ControlTrafico;
-
-public class SimulacionSemaforos {
-    public static void main(String[] args) {
-        Cruce cruce = new Cruce(10);
-        cruce.iniciar();
-    }
-}
-
